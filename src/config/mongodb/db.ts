6b672397e6e7e55f23ba6e178659b73810ff88b3
sengkimin <sengin.pse@gmail.com>
2025-04-24 10:33:42 +0700

    In

--- src/config/mongodb/db.ts
+++ src/config/mongodb/db.ts
@@ -20,33 +20,4 @@ export const connectMongoDB = async () => {
 };
 
 
-// import mongoose from "mongoose";
 
-// let isConnected = false;
-
-// export const connectMongoDB = async () => {
-//   if (isConnected) {
-//     console.log("🔁 Reusing existing MongoDB connection.");
-//     return;
-//   }
-
-//   try {
-//     const URI = "mongodb+srv://inseng:kimin12345@cluster0.jcvhu.mongodb.net/Inviteazy?retryWrites=true&w=majority";
-//     const connOptions = {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     } as mongoose.ConnectOptions;
-
-//     const conn = await mongoose.connect(URI, connOptions);
-//     isConnected = true;
-
-//     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
-//   } catch (error) {
-//     if (error instanceof Error) {
-//       console.error(`MongoDB Error: ${error.message}`);
-//     } else {
-//       console.error(`MongoDB Unknown Error: ${error}`);
-//     }
-//     process.exit(1);
-//   }
-// };
