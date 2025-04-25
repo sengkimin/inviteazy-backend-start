1aa09196f3ff397b06bc1fbbd9707dcace2db8a4
khonsreyvorleak5796-4 <sreyvorleak.khon@institute.pse.ngo>
2025-04-25 14:43:18 +0700

    save

--- src/config/postgresdb/db.ts
+++ src/config/postgresdb/db.ts
@@ -5,22 +5,14 @@ let poolInstance: Pool | null = null;
 export const connectPostgresDb = (): Pool => {
   if (!poolInstance) {
     poolInstance = new Pool({
-      user: "inviteazy",
+      user: "vorleak",
       host: "localhost",
       database: "mydb",
-      password: "12345678",
-      port: 5433,
+      password: "123456",
+      port: 5436,
     });
-    console.log(" Connected to Postgres!");
+    console.log("Connected to Postgres!");
   }
 
   return poolInstance;
-  const pool = new Pool({
-    user: "vorleak",
-    host: "localhost",
-    database: "mydb",
-    password: "123456",
-    port: 5436,
-  });
-  return pool;
 };
