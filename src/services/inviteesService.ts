45c782bcff3fbc50358c842607a813dec2a04ba6
sengkimin <sengin.pse@gmail.com>
2025-04-29 08:57:03 +0700

    new

--- src/services/inviteesService.ts
+++ src/services/inviteesService.ts
@@ -2,34 +2,35 @@ import { IInvitee, IInviteeRepository, IInviteeService } from "../interfaces/inv
 
 export class InviteeService implements IInviteeService {
   constructor(private inviteeRepository: IInviteeRepository) {}
-
-  async getinviteeById(inviteeId: string): Promise<IInvitee | null> {
-    return await this.inviteeRepository.findById(inviteeId);
+  getinviteeById(inviteeId: string): Promise<IInvitee | null> {
+    throw new Error("Method not implemented.");
   }
 
-  async updateCheckInStatus(event_id: string, user_id: string): Promise<IInvitee> {
-    return await this.inviteeRepository.updateCheckInStatus(event_id, user_id);
+  async getInviteeById(inviteeId: string): Promise<IInvitee | null> {
+    return this.inviteeRepository.findById(inviteeId);
   }
-  
+
+  async updateCheckInStatus(eventId: string, userId: string): Promise<IInvitee> {
+    return this.inviteeRepository.updateCheckInStatus(eventId, userId);
+  }
+
   async createInvitee(invitee: Omit<IInvitee, "id" | "created_at">): Promise<IInvitee> {
-    return await this.inviteeRepository.create(invitee);
+    return this.inviteeRepository.create(invitee);
   }
 
   async getAllInvitees(): Promise<IInvitee[]> {
-    return await this.inviteeRepository.findAll();
+    return this.inviteeRepository.findAll();
   }
 
   async getInviteesByEventId(eventId: string): Promise<IInvitee[]> {
-    return await this.inviteeRepository.findByEventId(eventId);
+    return this.inviteeRepository.findByEventId(eventId);
   }
 
   async updateInviteeStatus(inviteeId: string, status: string): Promise<IInvitee> {
-    return await this.inviteeRepository.updateStatus(inviteeId, status);
+    return this.inviteeRepository.updateStatus(inviteeId, status);
   }
 
   async updateCheckOutStatus(invitee: Omit<IInvitee, "id">, id: string): Promise<IInvitee | null> {
-    return await this.inviteeRepository.updateCheckOutStatus(invitee, id);
+    return this.inviteeRepository.updateCheckOutStatus(invitee, id);
   }
 }
-
-
